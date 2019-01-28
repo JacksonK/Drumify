c7264c7b1e12ea7a88b7687f68cfb3f1e0db6ea3
Jackson Kurtz <jkurtz678@gmail.com>
2019-01-27 19:09:55 -0800

    Initial Commit

---
+++ Drumify/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Drumify
+//
+//  Created by Jackson Kurtz on 1/27/19.
+//  Copyright © 2019 Jackson Kurtz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
