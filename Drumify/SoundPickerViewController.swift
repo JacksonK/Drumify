0e20304b29ff128d144038f509ba5c2194b68a8f
vernonchan <vernontchan@gmail.com>
2019-04-12 19:03:19 -0700

    the Sound Picker view controller now supports left and right swipes on the vertical lane to switch between Sound Picker and Sequencer views

--- Drumify/SoundPickerViewController.swift
+++ Drumify/SoundPickerViewController.swift
@@ -23,14 +23,64 @@ class SoundPickerViewController: UIViewController {
         return .landscapeLeft
     }
     
+    @IBOutlet weak var laneBarView: UIView!
+    @IBOutlet weak var rightOfLaneView: UIView!
+    @IBOutlet weak var leftOfLaneView: UIView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let value = UIInterfaceOrientation.landscapeLeft.rawValue
         UIDevice.current.setValue(value, forKey: "orientation")
         
+        initializeGestures()
+        
         //UIViewController.attemptRotationToDeviceOrientation()
         // Do any additional setup after loading the view.
+    }
+    
+    private func initializeGestures() {
+        //code from tutorial https://www.codevscolor.com/ios-adding-swipe-gesture-to-a-view-in-swift-4/
+        let left = UISwipeGestureRecognizer(target : self, action : #selector(self.leftSwipeOnLane))
+        left.direction = .left
+        self.laneBarView.addGestureRecognizer(left)
+        
+        let right = UISwipeGestureRecognizer(target : self, action : #selector(self.rightSwipeOnLane))
+        right.direction = .right
+        self.laneBarView.addGestureRecognizer(right)
+        
+    }
+    
+    //note: the 617 value for movement is hard-coded to work with iPhone 8 sized screens (for now).
+    @objc private func leftSwipeOnLane() {
+        print("swiped left")
+        
+        let laneCurrentPosition = self.laneBarView.layer.position
+        let rightCurrentPosition = self.rightOfLaneView.layer.position
+        let leftCurrentPosition = self.leftOfLaneView.layer.position
+        
+        UIView.animate(withDuration: 0.4) { 
+            self.laneBarView.layer.position = CGPoint(x: laneCurrentPosition.x - 617, y: laneCurrentPosition.y)
+            self.rightOfLaneView.layer.position = CGPoint(x: rightCurrentPosition.x - 617, y: rightCurrentPosition.y)
+            self.leftOfLaneView.layer.position = CGPoint(x: leftCurrentPosition.x - 617, y: leftCurrentPosition.y)
+            
+        }
+    }
+    
+    @objc private func rightSwipeOnLane() {
+        print("swiped right")
+        
+        let laneCurrentPosition = self.laneBarView.layer.position
+        let rightCurrentPosition = self.rightOfLaneView.layer.position
+        let leftCurrentPosition = self.leftOfLaneView.layer.position
+        
+        UIView.animate(withDuration: 0.4) { 
+            self.laneBarView.layer.position = CGPoint(x: laneCurrentPosition.x + 617, y: laneCurrentPosition.y)
+            self.rightOfLaneView.layer.position = CGPoint(x: rightCurrentPosition.x + 617, y: rightCurrentPosition.y)
+            self.leftOfLaneView.layer.position = CGPoint(x: leftCurrentPosition.x + 617, y: leftCurrentPosition.y)
+            
+        }
     }
     
 
