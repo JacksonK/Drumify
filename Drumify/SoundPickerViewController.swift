2026342e0024b86d9c2ef99c2439e9043c0e082d
vernonchan <vernontchan@gmail.com>
2019-04-26 17:07:37 -0700

    Lane Bar now moves not based on a hard coded value, but based on the size of the phone it is running on

--- Drumify/SoundPickerViewController.swift
+++ Drumify/SoundPickerViewController.swift
@@ -185,16 +185,20 @@ class SoundPickerViewController: UIViewController, UICollectionViewDataSource, U
     @objc private func leftSwipeOnLane() {
         print("swiped left")
         
+        let height = Constants.Screen.height
+        print(height)
+        
+        
         let laneCurrentPosition = self.laneBarView.layer.position
         let rightCurrentPosition = self.rightOfLaneView.layer.position
         let leftCurrentPosition = self.leftOfLaneView.layer.position
         let topCurrentPosition = self.topView.layer.position
         
         UIView.animate(withDuration: 0.4) { 
-            self.laneBarView.layer.position = CGPoint(x: laneCurrentPosition.x - 617, y: laneCurrentPosition.y)
-            self.rightOfLaneView.layer.position = CGPoint(x: rightCurrentPosition.x - 617, y: rightCurrentPosition.y)
-            self.leftOfLaneView.layer.position = CGPoint(x: leftCurrentPosition.x - 617, y: leftCurrentPosition.y)
-            self.topView.layer.position = CGPoint(x: topCurrentPosition.x - (617 / 2 - 5), y: topCurrentPosition.y)
+            self.laneBarView.layer.position = CGPoint(x: laneCurrentPosition.x - height, y: laneCurrentPosition.y)
+            self.rightOfLaneView.layer.position = CGPoint(x: rightCurrentPosition.x - height, y: rightCurrentPosition.y)
+            self.leftOfLaneView.layer.position = CGPoint(x: leftCurrentPosition.x - height, y: leftCurrentPosition.y)
+            self.topView.layer.position = CGPoint(x: topCurrentPosition.x - (height / 2 - 5), y: topCurrentPosition.y)
             
         }
     }
@@ -202,16 +206,20 @@ class SoundPickerViewController: UIViewController, UICollectionViewDataSource, U
     @objc private func rightSwipeOnLane() {
         print("swiped right")
         
+        let height = Constants.Screen.height
+        print(height)
+        
+        
         let laneCurrentPosition = self.laneBarView.layer.position
         let rightCurrentPosition = self.rightOfLaneView.layer.position
         let leftCurrentPosition = self.leftOfLaneView.layer.position
         let topCurrentPosition = self.topView.layer.position
         
         UIView.animate(withDuration: 0.4) { 
-            self.laneBarView.layer.position = CGPoint(x: laneCurrentPosition.x + 617, y: laneCurrentPosition.y)
-            self.rightOfLaneView.layer.position = CGPoint(x: rightCurrentPosition.x + 617, y: rightCurrentPosition.y)
-            self.leftOfLaneView.layer.position = CGPoint(x: leftCurrentPosition.x + 617, y: leftCurrentPosition.y)
-            self.topView.layer.position = CGPoint(x: topCurrentPosition.x + (617 / 2 - 5), y: topCurrentPosition.y)
+            self.laneBarView.layer.position = CGPoint(x: laneCurrentPosition.x + height, y: laneCurrentPosition.y)
+            self.rightOfLaneView.layer.position = CGPoint(x: rightCurrentPosition.x + height, y: rightCurrentPosition.y)
+            self.leftOfLaneView.layer.position = CGPoint(x: leftCurrentPosition.x + height, y: leftCurrentPosition.y)
+            self.topView.layer.position = CGPoint(x: topCurrentPosition.x + (height / 2 - 5), y: topCurrentPosition.y)
             
         }
     }
