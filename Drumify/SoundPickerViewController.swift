86d4f19651f243d285fc0274e79ff332a6e4c3db
vernonchan <vernontchan@gmail.com>
2019-04-28 17:56:05 -0700

    changed top bar animation travel distance to align with the collection view on RHS of sound picker view

--- Drumify/SoundPickerViewController.swift
+++ Drumify/SoundPickerViewController.swift
@@ -255,7 +255,7 @@ class SoundPickerViewController: UIViewController, UICollectionViewDataSource, U
             self.laneBarView.layer.position = CGPoint(x: laneCurrentPosition.x - toMove, y: laneCurrentPosition.y)
             self.rightOfLaneView.layer.position = CGPoint(x: rightCurrentPosition.x - toMove, y: rightCurrentPosition.y)
             self.leftOfLaneView.layer.position = CGPoint(x: leftCurrentPosition.x - toMove, y: leftCurrentPosition.y)
-            self.topView.layer.position = CGPoint(x: topCurrentPosition.x - (toMove / 2 - 5), y: topCurrentPosition.y)
+            self.topView.layer.position = CGPoint(x: topCurrentPosition.x - (toMove / 2 + 4 ), y: topCurrentPosition.y)
             
         }
     }
@@ -277,7 +277,7 @@ class SoundPickerViewController: UIViewController, UICollectionViewDataSource, U
             self.laneBarView.layer.position = CGPoint(x: laneCurrentPosition.x + toMove, y: laneCurrentPosition.y)
             self.rightOfLaneView.layer.position = CGPoint(x: rightCurrentPosition.x + toMove, y: rightCurrentPosition.y)
             self.leftOfLaneView.layer.position = CGPoint(x: leftCurrentPosition.x + toMove, y: leftCurrentPosition.y)
-            self.topView.layer.position = CGPoint(x: topCurrentPosition.x + (toMove / 2 - 5), y: topCurrentPosition.y)
+            self.topView.layer.position = CGPoint(x: topCurrentPosition.x + (toMove / 2 + 4), y: topCurrentPosition.y)
             
         }
     }
